5b29762849becab406d71f305e2adafdbec65d66
Anmolroan <anmol.kumar@masaischool.com>
2022-05-31 23:04:08 +0530

    class end

--- prabhanjan/firstday/index.js
+++ prabhanjan/firstday/index.js
@@ -1,5 +1,25 @@
 // node Js - Rayan Dahl
 // Javascript :Brendan Eich
 // const sum =require('./another');
-import sum from "./another"
-console.log(sum(4,5))
+// import sum from "./another"
+// console.log(sum(4,5))
+// const assert = require("assert");
+const fs = require("fs");
+const os = require("os");
+
+const path = require("path");
+// const data = fs.readFileSync("./test.txt",{encoding:"utf8"})
+// it is Sync function so untill line no 8 is not implemented the below code will not work
+// console.log(data)
+fs.readFile(path.join(".","test.txt"),{encoding:"utf8"},(err, data) => {
+    if(err) {
+        console.log("Error occurred ",err.message)
+    }else{
+        console.log(data)
+    }
+    
+      
+})
+console.log(os.cpus()[0].model);
+console.log(os.version())
+ 
