6e348edeef449afbe871f7c5b3b569472a4a2133
Anmolroan <anmol.kumar@masaischool.com>
2022-06-03 01:00:55 +0530

    1:35

--- prabhanjan/express_intro/index.js
+++ prabhanjan/express_intro/index.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 app.get("/", function(req, res){
     res.write("hello");
     res.end("world");
 })
-app.get("/", function(req, res){
-    res.send(JSON.stringify({a:"b",b:"c",c:"d"}));
-})
+// app.get("/", function(req, res){
+//     res.send(JSON.stringify({a:"b",b:"c",c:"d"}));
+// })
 app.post("/", function(req, res){
-    res.send(req.body);
+    fs.readFile("./db.json",{encoding: "utf8"},(err, data)=>{
+        const parsed = JSON.parse(data);
+        parsed.products =[...parsed.products,req.body];
+        fs.writeFile("./db.json",JSON.stringify(parsed),{encoding: "utf8"},()=>{
+            res.send("product created")
+        });
+    })
+   
+})
+
+// delete product
+app.delete("/product/:id", function(req, res){
+    fs.readFile("./db.json",{encoding: "utf8"},(err, data)=>{
+        const parsed = JSON.parse(data);
+        parsed.products = parsed.products.filter(el=>el.id!=1)
+        fs.writeFile("./db.json",JSON.stringify(parsed),{encoding: "utf8"},()=>{
+            res.send("product de;eted")
+        });
+    })
+})
+// put product
+app.put("//product/:id", function(req, res){
+    fs.readFile("./db.json",{encoding: "utf8"},(err, data)=>{
+        const parsed = JSON.parse(data);
+        parsed.products = parsed.products.filter(el=>el.id!=1)
+        fs.writeFile("./db.json",JSON.stringify(parsed),{encoding: "utf8"},()=>{
+            res.send("product update")
+        });
+    })
+
 })
 app.listen(1234, function(){
 console.log("server is running on port no 1234")
