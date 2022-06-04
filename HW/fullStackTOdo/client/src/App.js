a9e846e9a2217a8ad71d2f915ec8ab4e09a11fc6
Anmolroan <anmol.kumar@masaischool.com>
2022-06-05 01:24:21 +0530

    post and get is done

--- HW/fullStackTOdo/client/src/App.js
+++ HW/fullStackTOdo/client/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import {useState,useEffect} from "react"
 import './App.css';
-
+import { nanoid } from 'nanoid'
 function App() {
+  let str="";
+  const [todo,setTodo] = useState([]);
+  const [text,setText] = useState("");
+  const handleShow =(data) => {
+   setTodo( data);
+  
+   console.log(todo)
+  }
+  const handleAdd =(str) => {
+const payload ={
+  userId:nanoid(),
+name:text
+};
+fetch("http://localhost:1234",{
+  method: 'POST',
+  headers: {'Content-Type': 'application/json'},
+  body: JSON.stringify(payload)
+}).then((res)=>res.json()).then((data)=>setTodo(data));
+
+}
+  
+  useEffect(() =>{
+    
+      fetch("http://localhost:1234").then((res)=>res.json()).then((data)=>{
+        handleShow(data);
+      
+      })
+    
+    
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type="text" onChange={(e) => setText(e.target.value)}/>
+     <button onClick={()=>{handleAdd(str)}}>Add</button>
+     <div>{todo.map((el,i) => <div key ={i}>{el.name}</div>)}</div>
     </div>
   );
 }
---
+++ HW/fullStackTOdo/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const fs = require('fs')
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.get("/", function(req, res){
+    fs.readFile("./db.json",{encoding: "utf8"},(err, data)=>{
+        const parsed = JSON.parse(data);
+        res.send(parsed.todos)
+    })
+   
+})
+app.post("/", function(req, res){
+    fs.readFile("./db.json",{encoding: "utf8"},(err, data)=>{
+        const parsed = JSON.parse(data);
+        parsed.todos =[...parsed.todos,req.body];
+        fs.writeFile("./db.json",JSON.stringify(parsed),{encoding: "utf8"},()=>{
+            res.send(parsed.todos)
+        });
+    })
+   
+})
+app.listen(1234,() => {
+    console.log('listening on http://localhost:1234');
+});
